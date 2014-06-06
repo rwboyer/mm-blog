00397b23ddd05a6e9ca0e17380af3ca4607c8190
Robert W Boyer <rwboyer@mac.com>
2014-06-06 00:09:37 -0400

    cleaned up config.rb a bit

--- config.rb
+++ config.rb
@@ -32,9 +32,7 @@ ignore "/bower_components/"
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
-###
 # Helpers
-###
 
 # Ported Liquid tags
 
